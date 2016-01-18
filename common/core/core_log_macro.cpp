9497b5e9d550d989db30f66a7eed3a127194163b
wonktnodi <wonktnodiq@gmail.com>
2016-01-18 18:46:22 +0800

    change log path under android

--- common/core/core_log_macro.cpp
+++ common/core/core_log_macro.cpp
@@ -2,6 +2,9 @@
 
 #ifdef WIN32
 SingleLogStream core_log("core", BaseLogStreamInterface::debug);
+#elif defined(__ANDROID__)
+extern const char* ex_log_path;
+BaseLogStream core_log(ex_log_path, "core", BaseLogStreamInterface::debug);
 #else
 BaseLogStream core_log("core", BaseLogStreamInterface::debug);
 #endif
--- common/rudp/rudp_log_macro.cpp
+++ common/rudp/rudp_log_macro.cpp
@@ -1,5 +1,12 @@
 #include "rudp/rudp_log_macro.h"
 
+#ifdef __ANDROID__
+extern const char* ex_log_path;
+SingleLogStream rdup_log(ex_log_path, "rudp", SingleLogStream::debug);
+SingleLogStream rdup_recv_log(ex_log_path, "rudp_recv", SingleLogStream::debug);
+SingleLogStream rdup_send_log(ex_log_path, "rudp_send", SingleLogStream::debug);
+#else
 SingleLogStream rdup_log("rudp", SingleLogStream::debug);
 SingleLogStream rdup_recv_log("rudp_recv", SingleLogStream::debug);
 SingleLogStream rdup_send_log("rudp_send", SingleLogStream::debug);
+#endif
